29c27193d8997130f915b01ef16019b21bc98cb1
RakeshSingh2811 <128033460+Rakesh-Es-dev@users.noreply.github.com>
2023-05-09 17:11:37 +0530

    complete app setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import Tooltip from "./Tooltip";
-
+import "../styles/index.css";
 function App() {
   return (
     <div className="App">
-      <h1 style={{ textAlign: "center" }}>Tooltip Sample</h1>
-      <Tooltip postion="right" text="information"></Tooltip>
+      <h1 style={{ textAlign: "center" }}>React Tooltip</h1>
+      <div className="tooltipContainer">
+        <Tooltip text="default tooltip"></Tooltip>
+        <Tooltip position="Right" text="Right tooltip"></Tooltip>
+        <Tooltip position="Top" text="Top tooltip"></Tooltip>
+        <Tooltip position="Bottom" text="Bottom tooltip"></Tooltip>
+        <Tooltip position="Left" text="Left tooltip"></Tooltip>
+      </div>
     </div>
   );
 }
--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -2,15 +2,21 @@ import React from "react";
 import styles from "../styles/tooltip.module.css";
 class Tooltip extends React.Component {
   render() {
-    console.log(this.props);
+    const { position, text } = this.props;
+    let className = "";
+    if (position === "Right") {
+      className = "tooltipTextRight";
+    } else if (position === "Bottom") {
+      className = "tooltipTextBottom";
+    } else if (position === "Left") {
+      className = "tooltipTextLeft";
+    } else {
+      className = "tooltipTextTop";
+    }
     return (
-      <div className={styles.container}>
-        <div className={styles.tooltipContainer}>
-          <div className={styles.tooltipTarget}>
-            Press
-            <div className={styles.tooltipTextTop}>Information</div>
-          </div>
-        </div>
+      <div className={styles.tooltipTarget}>
+        Press
+        <div className={className}>{text}</div>
       </div>
     );
   }
